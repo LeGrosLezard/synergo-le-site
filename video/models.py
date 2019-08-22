c4871f205652057758c5f5ca6d9bd0497f44d1dd
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-22 16:42:13 +0200

    Add files via upload

---
+++ video/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class video_upload(models.Model):
+    docfile = models.FileField(upload_to='video_upload')
+
+
---
+++ video/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def telechargement_video(request):
+    return render(request, 'telechargement_video.html')
