b79c8ed03e4e027b75bcf37b44ccf9dd8a11f5f2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-22 20:20:33 +0200

    Update views.py

--- video/views.py
+++ video/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
 
+#We importing form and model from
+#models.py and forms.py
+#We return the form,
+#We recup the post and if it's valid we uploading it
+#on the MEDIA folder.
+
+
+from .forms import video_upload_form
+from .models import video_upload
 def telechargement_video(request):
-    return render(request, 'telechargement_video.html')
+
+    pseudo = request.user
+
+    form = video_upload_form(request.POST, request.FILES)
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            print("ouiiiiiiiiiii")
+
+            newdoc = video_upload(docfile = request.FILES['docfile'])
+            newdoc.save()
+           
+            print("coucou")
+
+        
+    return render(request, 'telechargement_video.html', {"form":form})
