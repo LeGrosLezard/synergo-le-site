c3f9cc50cf6aafc0a2caf7e5b7011d646c530f2a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-23 19:52:27 +0200

    Update users_video.py

--- database/video/users_video.py
+++ database/video/users_video.py
@@ -6,11 +6,10 @@ from .config import HOST
 from .config import PASSWORD
 
 
-def insertion_video_user(pseudo):
-    """Here we insert into the database
-    of the current user
-    his video uploaded"""
 
+def current_user_video(pseudo, video_name):
+    """We insert video/user"""
+    
     #We call psycopg2 (database from heroku serveur)
     conn = psycopg2.connect(database=DATABASE,
                             user=USER,
@@ -19,17 +18,31 @@ def insertion_video_user(pseudo):
 
     cur = conn.cursor()
 
-    cur.execute("""select id, nom, prenom, lieu_habitation, fixe,
-                portable, email from users
-                where pseudo = '{}';""".format(pseudo))
+    cur.execute("""
+                INSERT INTO VIDEO
+                (video_name, pseudo)
+                VALUES(%s, %s);
+                """, (str(video_name), str(pseudo)))
 
     conn.commit() 
+    #In real word -> we insert in the table of the database
+    #who's called VIDEO the name of video and of user
 
-    rows = cur.fetchall()
-    liste = [i for i in rows]
 
-    return liste[0][0], liste[0][1], liste[0][2], liste[0][3],\
-           liste[0][4], liste[0][5], liste[0][6]
+
+
+
+
+
+##    rows = cur.fetchall()
+##    liste = [i for i in rows]
+##
+##    print(liste)
+
+
+
+
+
 
 
 
