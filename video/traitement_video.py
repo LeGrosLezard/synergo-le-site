956188d87786771fc5b9e1f2a5f68b7dc9701384
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-28 00:42:50 +0200

    Update traitement_video.py
    les saut de sourcile,  faudra demandé le mec est loin ? prés ? et faire le zoom faut faire de l'ia mais c correct jcrois niquez vos races uploadez vos vidéo svp comme ca personne peut m'engueuler

--- video/traitement_video.py
+++ video/traitement_video.py
@@ -7,81 +7,90 @@ import os
 
 def essais(frame, faceCascade, eyesCascade, LISTE):
     
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
+        frame,
+        scaleFactor=3.0,
         minNeighbors=1,
-        minSize=(30, 30),
+        minSize=(60, 100),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
     
-
-    counter = 0
-    droite = 0
-    gauche = 0
-    x_droite = 0
-    ok = ""
     for x1, y1, w1, h1 in faces:
         
         h1 = h1.tolist()
-        h1 = int(round(h1/1.2))
+        h1 = int(round(h1/2))
 
-        roi_gray = gray[y1:y1+h1, x1:x1+w1]
-        roi_color = frame[y1:y1+h1, x1:x1+w1]
+        roi_gray = frame[y1:y1+h1, x1:x1+w1]
+
+        cv2.rectangle(frame, (x1,y1), (x1+w1, y1+h1), 2)
         
         eyes = eyesCascade.detectMultiScale(roi_gray,
-            scaleFactor=1.3,
+            scaleFactor=1.6,
             minNeighbors=1,
             minSize=(40, 40),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
 
 
+        essais1(eyes, roi_gray, frame, LISTE)
 
+
+
+
+def essais1(eyes, roi_gray, frame, LISTE):
+
+    #Sometimes eyes are un sur l'autre
+    eye = 0
+    #Sometimes eyes switch
+    compteur = 0
+    gauche = 0
+    droite = 0
+    x_droite = 0
+    ok = ""
+    
+    if 1 < len(eyes) <= 2:
         for x, y, w, h in eyes:
+            if x + 5 >= eye <= x - 5:
+                eye += x
 
-            if len(eyes) == 1:
-                pass
-                #on ne trouve qu'un oeil ? on passe
-            
-            else:
-            
-                y = y.tolist()
-                x = x.tolist()
-
-                if counter == 0:
-                    cv2.rectangle(roi_color, (x,y), (x+w, y+h), 2)
+                if compteur == 0:
+                    cv2.rectangle(roi_gray, (x,y), (x+w, y+h),(255), 2)
                     gauche = y
-
                     
-                if counter == 1:
-             
-                    LISTE.append(x)
-                    cv2.rectangle(roi_color, (x,y), (x+w, y+h),(255, 0, 0), 2)
+                else:
+                    cv2.rectangle(roi_gray, (x,y), (x+w, y+h),(0), 2)
                     droite = y
                     x_droite = x
+
                     
-                counter += 1
+            compteur += 1
 
 
-                if gauche == 0 or droite == 0:
-                    pass
-                else:
-                    
-                    if gauche < droite - 10 and x_droite > (sum(LISTE) / len(LISTE)) + 15:
-                        print("gauche")
-                        ok = True
+    if gauche == 0 or droite == 0:
+        pass
+    else:
+        if droite - 20 < gauche < droite - 5:
+            print("gauche")
+            ok = True
 
-                    if ok is True:
-                        pass
-                    else:
-                        if gauche < droite - 10:
-                            print("droite")
+        if ok != True:
+            if droite + 15 > gauche > droite + 5:
+                print("droite")
 
 
-                cv2.imshow('VIDEO', frame)
+      
+    cv2.imshow('VIDEO', frame)
+
+
+
+
+
+
+
+
+
+
 
 
 
