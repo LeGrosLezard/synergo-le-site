d3b080fb489d1b10c9e685ed98d15968277c484a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-27 03:19:50 +0200

    Update views.py

--- synergo/views.py
+++ synergo/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def garde(request):
+    return render(request, 'garde.html')
+
+def transition(request):
+    return render(request, 'transition.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def essais_home(request):
+    return render(request, 'essais_home.html')
