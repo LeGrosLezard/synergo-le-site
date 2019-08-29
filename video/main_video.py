26d9e77f10b9a5234e67db77e0a0dc4dc5c03f12
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-29 16:17:51 +0200

    Update main_video.py
    j'y ai pas pensé a cause de ca mince et j'arrive pas a en faire une fonction

--- video/main_video.py
+++ video/main_video.py
@@ -4,24 +4,39 @@ from PIL import Image
 import os
 
 
+SUBSTRACTOR = cv2.createBackgroundSubtractorMOG2(history=100,
+                                                varThreshold=50,
+                                                detectShadows=True)
+
+SUBSTRACTOR1 = cv2.createBackgroundSubtractorMOG2(history=100,
+                                                varThreshold=50,
+                                                detectShadows=True)
 
 
 def video_capture_visage():
 
 
-    video = cv2.VideoCapture("VIDEO.mp4")
+    video = cv2.VideoCapture("video_jb.mp4")
     faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
     
     MOUVEMENT = []
     c = 0
+    d = []
+    e = 0
+    f = []
+    
     
     while(True):
 
+
         ret, frame_visage = video.read()
         frame_visage = cv2.resize(frame_visage, (1200, 1000))
+        frame_visage1 = cv2.resize(frame_visage, (1200, 1000))
+
+
+        
 
         gray = cv2.cvtColor(frame_visage, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.threshold(gray, 130, 255, 1)[1]
 
         #-------------------------------------------------------------1
         faces = faceCascade.detectMultiScale(
@@ -31,89 +46,68 @@ def video_capture_visage():
 
         for x, y, w, h in faces:
             
-    
+            cv2.rectangle(gray, (x, y), (x+w, y +h), 2)
             #---------------------------------------------------------2
 
             MOUVEMENT.append(x)
 
-
+            
 
             try:
-                if MOUVEMENT[-2] - 10 < x > MOUVEMENT[-2] + 10:
-                    print("oui")
+
+
+                #---------------------------------------------------------
+
+                #milieu
+                #cv2.rectangle(gray, (x + int(round(w/3)), y - int(round(150 * 100 / h))), (x + int(round(w/3)) * 2, y - int(round(80 * 100 / h))), 2)
+                crop = gray[y - int(round(150 * 100 / h)):y - int(round(80 * 100 / h)), x + int(round(w/3)):x + int(round(w/3)) * 2]
+                mask = SUBSTRACTOR.apply(crop)
+
+
+                liste = []
+
+                for i in mask:
+                    for j in i:
+                        liste.append(j)
+
+                if sum(liste) == 0:
+                    c = 0
+                    d = []
+
+                elif c > 5:
+                    if sum(d) > 1000000:
+                        #print("mouvement")
+                        pass
                 else:
-
-                    #---------------------------------------------------------
-
-                    def coloriage(a, b, c, d):
-                        for i in range(a, b):
-                            for j in range(c, d):
-                                thresh[i ,j] = 255
-
-                    #milieu
-                    cv2.rectangle(thresh, (x + int(round(w/3)), y - int(round(150 * 100 / h))), (x + int(round(w/3)) * 2, y - int(round(80 * 100 / h))), 2)
-                    coloriage(y - int(round(150 * 100 / h)), y - int(round(80 * 100 / h)), x + int(round(w/3)),x + int(round(w/3)) * 2)
+                    c += 1
+                    d.append(sum(liste))
 
 
+                #pattes
+                cv2.rectangle(gray, (x - 20, y - int(round(110 * 100 / h))), (x + 30, y - int(round(50 * 100 / h))), 2)
+                crop1 = gray[y - int(round(110 * 100 / h)):y - int(round(50 * 100 / h)), x - 20:x + 30]
+                mask = SUBSTRACTOR1.apply(crop1)
+                liste1 = []
+
+                for i in mask:
+                    for j in i:
+                        liste1.append(j)
+
+                if sum(liste1) == 0:
+                    e = 0
+                    f = []
+
+                elif c > 5:
+                    if sum(f) > 1000000:
+                        print("mouvement1")
+ 
+                else:
+                    e += 1
+                    f.append(sum(liste1))
 
 
 
 
-                    
-                    #pattes
-                    cv2.rectangle(thresh, (x - 20, y - int(round(110 * 100 / h))), (x + 30, y - int(round(50 * 100 / h))), 2)
-                    coloriage(y - int(round(110 * 100 / h)), y - int(round(50 * 100 / h)), x - 20, x + 30)
-
-                    
-                    cv2.rectangle(thresh, (x + w - 20, y - int(round(100 * 100 / h))), (x + w + 30, y - int(round(40 * 100 / h))), 2)
-                    coloriage(y - int(round(100 * 100 / h)), y - int(round(40 * 100 / h)), x + w - 20, x + w + 30)
-                            
-                    #téco
-                    cv2.rectangle(gray, (x - w - 30, y), (x - 60, y + h - 30), 3)
-                    coloriage(y, y + h - 30, x - w - 30, x - 60)
-                    cv2.rectangle(gray, (x + w + 60, y), (x + w * 2 + 30, y + h - 30), 3)
-                    coloriage(y, y + h - 30, x + w + 60, x + w * 2 + 30)
-
-                            
-                    #chebou
-                    cv2.rectangle(gray,(x + int(round(w/3)), y + h - 20), (x + int(round(w/3)) * 2, y + h - 5), 1)
-                    coloriage(y + h - 20, y + h - 5, x + int(round(w/3)), x + int(round(w/3)) * 2)
-
-                            
-                    #menton
-                    cv2.rectangle(gray,(x + int(round(w/3)), y + h + 10), (x + int(round(w/3)) * 2, y + h + 25), 1)
-                    coloriage(y + h + 10, y + h + 25, x + int(round(w/3)), x + int(round(w/3)) * 2)
-
-                            
-                    #buste
-                    cv2.rectangle(gray,(x, y + h + 120), (x + w, y + h + 180), 1)
-                    coloriage(y + h + 120, y + h + 180, x, x + w)
-
-                            
-                    #epaul
-                    cv2.rectangle(gray, (x - 50, y + h + 20), (x + 30, y + h + 60), 1)
-                    coloriage(y + h + 20, y + h + 60, x - 50, x + 30)
-                    
-                    cv2.rectangle(gray,(x + w - 30, y + h + 20), (x + w + 30, y + h + 60), 1)
-                    coloriage(y + h + 20, y + h + 60, x + w - 30,x + w + 30)
-                            
-                    #front
-                    cv2.rectangle(gray, (x + 30, y - int(round(30 * 100 / h))), (x + w - 30, y - int(round(-40 * 100 / h))), 2)
-                    coloriage(y - int(round(30 * 100 / h)), y - int(round(-40 * 100 / h)), x + 30, x + w - 30)
-                            
-                    #tempes
-                    cv2.rectangle(gray,(x - 40, y - 20), (x, y + 40), 1)
-                    coloriage(y - 20, y + 40, x - 40, x)
-                    
-                    cv2.rectangle(gray,(x + w, y + 40), (x + w + 40, y - 20 ), 1)
-                    coloriage(y + 40, y - 20, x + w, x + w + 40)
-
-                    cv2.rectangle(gray,(x - 40, y + 70), (x, y + 110), 1)
-                    coloriage(y + 70, y + 110, x - 40, x)
-
-                    
-                    cv2.rectangle(gray,(x + w + 40, y + 70), (x + w, y + 110), 1)
-                    coloriage(y + 70, y + 110, x + w + 40, x + w)
                     
                         
             except:
@@ -124,7 +118,8 @@ def video_capture_visage():
             
   
 
-        cv2.imshow('FACE', thresh)
+        cv2.imshow('FACE', gray)
+        
 
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -133,7 +128,7 @@ def video_capture_visage():
         if len(MOUVEMENT) > 10:
             del MOUVEMENT[:8]
 
-        c += 1
+
     video.release()
     cv2.destroyAllWindows()
 
