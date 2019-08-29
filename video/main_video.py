e1a375edd4c07ebbc842dda6699376a71829db6b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-08-29 03:05:19 +0200

    Update main_video.py

--- video/main_video.py
+++ video/main_video.py
@@ -21,6 +21,7 @@ def video_capture_visage():
         frame_visage = cv2.resize(frame_visage, (1200, 1000))
 
         gray = cv2.cvtColor(frame_visage, cv2.COLOR_BGR2GRAY)
+        thresh = cv2.threshold(gray, 130, 255, 1)[1]
 
         #-------------------------------------------------------------1
         faces = faceCascade.detectMultiScale(
@@ -29,11 +30,13 @@ def video_capture_visage():
             flags=cv2.CASCADE_SCALE_IMAGE)
 
         for x, y, w, h in faces:
-            cv2.rectangle(gray, (x, y), (x + w, y + h), (0,0,0), 2)
+            
     
             #---------------------------------------------------------2
 
             MOUVEMENT.append(x)
+
+
 
             try:
                 if MOUVEMENT[-2] - 10 < x > MOUVEMENT[-2] + 10:
@@ -41,28 +44,15 @@ def video_capture_visage():
                 else:
 
                     #---------------------------------------------------------
-                    print("non")
-                    y = y - int(round(150 * 100 / h))
-                    y1 = y - int(round(80 * 100 / h))
-                    carre = int(round(w/3))
 
-                    cv2.rectangle(gray, (x + carre, y), (x + carre * 2, y1), (0), 2)
+                    def coloriage(a, b, c, d):
+                        for i in range(a, b):
+                            for j in range(c, d):
+                                thresh[i ,j] = 255
 
-                    print(y, y1, carre)
-
-                    
-
-
-
-
-
-
-
-
-
-
-
-
+                    #milieu
+                    cv2.rectangle(thresh, (x + int(round(w/3)), y - int(round(150 * 100 / h))), (x + int(round(w/3)) * 2, y - int(round(80 * 100 / h))), 2)
+                    coloriage(y - int(round(150 * 100 / h)), y - int(round(80 * 100 / h)), x + int(round(w/3)),x + int(round(w/3)) * 2)
 
 
 
@@ -70,6 +60,62 @@ def video_capture_visage():
 
 
                     
+                    #pattes
+                    cv2.rectangle(thresh, (x - 20, y - int(round(110 * 100 / h))), (x + 30, y - int(round(50 * 100 / h))), 2)
+                    coloriage(y - int(round(110 * 100 / h)), y - int(round(50 * 100 / h)), x - 20, x + 30)
+
+                    
+                    cv2.rectangle(thresh, (x + w - 20, y - int(round(100 * 100 / h))), (x + w + 30, y - int(round(40 * 100 / h))), 2)
+                    coloriage(y - int(round(100 * 100 / h)), y - int(round(40 * 100 / h)), x + w - 20, x + w + 30)
+                            
+                    #téco
+                    cv2.rectangle(gray, (x - w - 30, y), (x - 60, y + h - 30), 3)
+                    coloriage(y, y + h - 30, x - w - 30, x - 60)
+                    cv2.rectangle(gray, (x + w + 60, y), (x + w * 2 + 30, y + h - 30), 3)
+                    coloriage(y, y + h - 30, x + w + 60, x + w * 2 + 30)
+
+                            
+                    #chebou
+                    cv2.rectangle(gray,(x + int(round(w/3)), y + h - 20), (x + int(round(w/3)) * 2, y + h - 5), 1)
+                    coloriage(y + h - 20, y + h - 5, x + int(round(w/3)), x + int(round(w/3)) * 2)
+
+                            
+                    #menton
+                    cv2.rectangle(gray,(x + int(round(w/3)), y + h + 10), (x + int(round(w/3)) * 2, y + h + 25), 1)
+                    coloriage(y + h + 10, y + h + 25, x + int(round(w/3)), x + int(round(w/3)) * 2)
+
+                            
+                    #buste
+                    cv2.rectangle(gray,(x, y + h + 120), (x + w, y + h + 180), 1)
+                    coloriage(y + h + 120, y + h + 180, x, x + w)
+
+                            
+                    #epaul
+                    cv2.rectangle(gray, (x - 50, y + h + 20), (x + 30, y + h + 60), 1)
+                    coloriage(y + h + 20, y + h + 60, x - 50, x + 30)
+                    
+                    cv2.rectangle(gray,(x + w - 30, y + h + 20), (x + w + 30, y + h + 60), 1)
+                    coloriage(y + h + 20, y + h + 60, x + w - 30,x + w + 30)
+                            
+                    #front
+                    cv2.rectangle(gray, (x + 30, y - int(round(30 * 100 / h))), (x + w - 30, y - int(round(-40 * 100 / h))), 2)
+                    coloriage(y - int(round(30 * 100 / h)), y - int(round(-40 * 100 / h)), x + 30, x + w - 30)
+                            
+                    #tempes
+                    cv2.rectangle(gray,(x - 40, y - 20), (x, y + 40), 1)
+                    coloriage(y - 20, y + 40, x - 40, x)
+                    
+                    cv2.rectangle(gray,(x + w, y + 40), (x + w + 40, y - 20 ), 1)
+                    coloriage(y + 40, y - 20, x + w, x + w + 40)
+
+                    cv2.rectangle(gray,(x - 40, y + 70), (x, y + 110), 1)
+                    coloriage(y + 70, y + 110, x - 40, x)
+
+                    
+                    cv2.rectangle(gray,(x + w + 40, y + 70), (x + w, y + 110), 1)
+                    coloriage(y + 70, y + 110, x + w + 40, x + w)
+                    
+                        
             except:
                 pass
 
@@ -78,7 +124,7 @@ def video_capture_visage():
             
   
 
-        cv2.imshow('FACE', gray)
+        cv2.imshow('FACE', thresh)
 
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
