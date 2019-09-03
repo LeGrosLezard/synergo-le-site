d69f77811687c9b17b45740c42656b0c0750da1c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-09-03 21:10:49 +0200

    Update essais1.py

--- video/essais1.py
+++ video/essais1.py
@@ -1,362 +1,150 @@
+import os
+import sys
+import time
 import numpy as np
 import cv2
 from PIL import Image
 import operator
 from collections import defaultdict
-import time
+from essais6 import *
 
 
 
+DIRECTION_VERTICALE = []
+HAND = []
+MOUVEMENT = []
 
-#ICI mouvement detecteur --------------------------------------
+SUBSTRACTOR9 = cv2.createBackgroundSubtractorMOG2(history=100,
+                                                varThreshold=50,
+                                                detectShadows=True)
+l = []
+l1 = []
+l2 = []
+l3 = []
+l4 = []
+l5 = []
+l6 = []
+l7 = []
+l8 = []
+l9 = []
+l10 = []
+l11 = []
+l12 = []
+l13 = []
+l14 = []
+l15 = []
 
-def original_traitement(cap, kernel_blur):
+ll1 = [[], [], [], []]
+ll2 = [[], [], [], []]
+ll3 = [[], [], [], []]
+ll4 = [[], [], [], []]
+ll5 = [[], [], [], []]
+ll6 = [[], [], [], []]
+ll7 = [[], [], [], []]
+ll8 = [[], [], [], []]
+ll9 = [[], [], [], []]
+ll10 = [[], [], [], []]
+ll11 = [[], [], [], []]
+ll12 = [[], [], [], []]
+ll13 = [[], [], [], []]
+ll14 = [[], [], [], []]
+ll15 = [[], [], [], []]
 
-    ret, originale=cap.read()
-    originale = cv2.resize(originale, (800, 600))
-    originale=cv2.cvtColor(originale, cv2.COLOR_BGR2GRAY)
-    originale=cv2.GaussianBlur(originale, (kernel_blur, kernel_blur), 0)
-    kernel_dilate=np.ones((10, 10), np.uint8)
 
-    return originale, kernel_dilate
-
-
-def to_mask(frame, gray, originale, kernel_blur, seuil, kernel_dilate):
     
-    gray=cv2.GaussianBlur(gray, (kernel_blur, kernel_blur), 0)
-    mask=cv2.absdiff(originale, gray)
-    mask=cv2.threshold(mask, seuil, 255, cv2.THRESH_BINARY)[1]
-    mask=cv2.dilate(mask, kernel_dilate, iterations=5)
-    contours, nada=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    frame_contour=frame.copy()
-
-    return contours, frame_contour
 
 
 
 
-#----------------------------------------------Meme fonction
-def bras_mouvement(air, frame, x, y):
-    proba = 100
 
-    if air > 9500:
-        return False
-    else:
-        return True
 
+
+cap=cv2.VideoCapture("yo.mp4")
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+
+
+counter = 0
+kernel_blur=13
+seuil=10
+surface=1000
+
+hand_detection = False
+
+
+while True:
 
     
-def contour(frame, contours, surface, frame_contour):
+
     
-    for c in contours:
-        if cv2.contourArea(c) < 100000:
-
-            cv2.drawContours(frame_contour, [c], 0, (0, 255, 0), 5)
-            if cv2.contourArea(c) < surface:
-                continue
-
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            taille_area = bras_mouvement(cv2.contourArea(c), frame, x, y)
-
-            
-            return x, y, w, h, taille_area
-
-    return None, None, None, None, False
-
-#----------------------------------------------Meme fonction
-
-
-
-
-
-
-
-
-#ICI skin detecteur--------------------------------------------------------
-def face_detector(faceCascade, gray, frame):
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
-        minNeighbors=1,
-        minSize=(60, 100),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2)
-
-        frame1 = frame[y+10:y+h-10, x+30:x+w-35]
-
-        return frame1, x, y, w, h
-
-
-
-def most_pixel(c, frame1):
-
-
-    dico = {}
-    img = Image.fromarray(frame1)
-    for value in img.getdata():
-        if value in dico.keys():
-            dico[value] += 1
-        else:
-            dico[value] = 1
-            
-    sorted_x = sorted(dico.items(), key=operator.itemgetter(1), reverse=True)
+    ret, frame =cap.read()
+    frame = cv2.resize(frame, (800, 600))
+    frame_movement = cv2.resize(frame, (800, 600))
+    frame_area = cv2.resize(frame, (800, 600))
     
-    a = sorted_x[0][0][0] + 20, sorted_x[0][0][1] + 20, sorted_x[0][0][2] + 40
-    b = sorted_x[-1][0][0], sorted_x[-1][0][1], sorted_x[-1][0][2]
 
-    return a, b
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
+    originale, kernel_dilate = original_traitement(cap, kernel_blur)
+    contours, frame_contour = to_mask(frame_movement, gray, originale, kernel_blur, seuil, kernel_dilate)
+    x_mov, y_mov, w_mov, h_mov, taille_area = contour(frame_movement, contours, surface, frame_contour)
 
-def detector_W_px(skinMask, y_mov, h_mov, x_mov, w_mov, frame):
-    counter_Wpx = 0
-
-    #On verifie que l'interieur de la détection est une zone de peau.
-    detector_skin = skinMask[y_mov:y_mov+h_mov, x_mov:x_mov+w_mov]
-    area_zone = frame[y_mov:y_mov+h_mov, x_mov:x_mov+w_mov]
-
-    for i in range(detector_skin.shape[0]):
-        for j in range(detector_skin.shape[1]):
-            if detector_skin[i, j] == 255:
-                counter_Wpx+=1
-   
-    return counter_Wpx
-
-
-
-def skin_mask(frame, frame1, frame_movement, a, b, c, x, y, w, h,
-              x_mov, y_mov, w_mov, h_mov, taille_area,
-              DIRECTION_VERTICALE, HAND,
-              hand_detection):
-
-    #On recoit si c'est un grand mouvement ou un petit
-    # -> CONTOUR()
-
-    if c > 5:
-
-        skinMask = cv2.inRange(frame, np.array([b], dtype = "uint8"), np.array([a], dtype = "uint8"))
+    
+    
+    
+    try:
+        frame1, x, y, w, h = face_detector(faceCascade, gray, frame)
         
-        #PETIT MOUVEMENT
-        if taille_area is True:
+        if counter == 5:
+            UPPER, LOWER = most_pixel(counter, frame1)
 
-            frame_detector = skinMask[y_mov:y_mov+h_mov, x_mov:x_mov+w_mov]
-
-            #VISAGE
-            if x - 20 < x_mov and int(round(y+h*1.5)) > y_mov+h_mov:
-                if hand_detection is True:
-                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "tete")
-                    print(DIRECTION_VERTICALE[-1], x_mov)
-                m = False
+        if counter > 5:
+            skinMask, hand_detection = skin_mask(frame, frame1, frame_movement, UPPER, LOWER,
+                                                 counter, x, y, w, h,
+                                                 x_mov, y_mov, w_mov, h_mov, taille_area,
+                                                 DIRECTION_VERTICALE, HAND,
+                                                 hand_detection)
 
 
-            #AUTRE
-            else:
-
-                counter_Wpx = detector_W_px(skinMask, y_mov, h_mov, x_mov, w_mov, frame)
-
-                #NON MAIN
-                if counter_Wpx == 0:
-                    cv2.putText(frame_movement, str("non main"),
-                            (x_mov, y_mov), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255,255,255),1,cv2.LINE_AA)
-                    m = False
-    
-
-                #MAIN
-                elif counter_Wpx > 0:
-
-                    #BAS
-                    if y_mov+h_mov > int(round(600*80/100)):
-                        if hand_detection is True:
-                            m = False
-                            if DIRECTION_VERTICALE[-1] - x_mov < 250:
-                                print(DIRECTION_VERTICALE[-1], x_mov)
-                                #droite
-                                if x_mov < x+w/2:
-                                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "bas gauche")
-                                #gauche
-                                elif x_mov > x+w/2:
-                                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "bas droite")
-
-
-                    #MILIEU
-                    elif y_mov+h_mov > int(round(600*50/100)) and y_mov+h_mov < int(round(600*80/100)):
-                        if hand_detection is True:
-                            m = False
-                            if DIRECTION_VERTICALE[-1] - x_mov < 250:
-                                print(DIRECTION_VERTICALE[-1], x_mov)
-                                #droite
-                                if x_mov < x+w/2:
-                                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "milieu gauche")
-                                #gauche
-                                elif x_mov > x+w/2:
-                                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "milieu droite")
-
-
-
-                    #HAUT
-                    elif y_mov+h_mov < int(round(600*50/100)):
-                        if hand_detection is True:
-                            m = False
-                            if DIRECTION_VERTICALE[-1] - x_mov < 250:
-                                print(DIRECTION_VERTICALE[-1], x_mov)
-                                #droite
-                                if x_mov < x+w/2:
-                                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "haut gauche")
-                                #gauche
-                                elif x_mov > x+w/2:
-                                    hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, "haut droite")
-
-                                
-
-
-        #GRAND MOUVEMENT
-        elif taille_area is False:
-
-            #ZONE TETE
-            if x - 30 < x_mov and int(round(y+h+45)) > y_mov+h_mov:
-                cv2.putText(frame_movement, str("TETE"),
-                            (x_mov, y_mov), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255,255,255),1,cv2.LINE_AA)
-
-
-            #AUTRE ZONE
-            else:
-            
-                #BAS
-                if y_mov+h_mov > int(round(600*80/100)):
-                    #droite
-                    if x_mov < x+w/2:
-                        m = mouvement_direction(frame_movement, DIRECTION_VERTICALE, y_mov, h_mov,
-                                                x_mov, "bas gauche")
-                    #gauche
-                    elif x_mov > x+w/2:
-
-                        m = mouvement_direction(frame_movement,DIRECTION_VERTICALE, y_mov, h_mov,
-                                                x_mov, "bas droite")
-
-
-
-                #MILLIEU
-                elif y_mov+h_mov > int(round(600*50/100)) and y_mov+h_mov < int(round(600*80/100)):
-                    #droite
-                    if x_mov < x+w/2:
-                        m = mouvement_direction(frame_movement, DIRECTION_VERTICALE, y_mov, h_mov,
-                                                x_mov, "milieu gauche")
-                    #gauche
-                    elif x_mov > x+w/2:
-                        m = mouvement_direction(frame_movement, DIRECTION_VERTICALE, y_mov, h_mov,
-                                                x_mov, "milieu droite")
-
-
-
-
-                #HAUT
-                elif y_mov+h_mov <= int(round(600*50/100)):
-                    #droite
-                    if x_mov < x+w/2:
-                        m = mouvement_direction(frame_movement, DIRECTION_VERTICALE, y_mov, h_mov,
-                                                DIRECTION_HORIZONTALE, x_mov, "haut gauche")
-                    #gauche
-                    elif x_mov > x+w/2:
-                        m = mouvement_direction(frame_movement, DIRECTION_VERTICALE, y_mov, h_mov,
-                                                x_mov, "haut droite")
-
-
-   
-        return skinMask, m
-
-
-
-
-
-def hand_movement(frame_movement, HAND, x_mov, y_mov, h_mov, zone):
-    
-    try:
-        #print(HAND[-1], zone)
-        print("main", zone)
-    except:
-        pass
-
-    cv2.putText(frame_movement, str("main " + " " + str(zone)),
-                (x_mov, y_mov), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255,255,255),1,cv2.LINE_AA)
-
-    HAND.append(y_mov+h_mov)
-
-
-
-
-def mouvement_direction(frame_movement, DIRECTION_VERTICALE, y, h, x, zone):
-
-
-    try:
-        print("mouvement", zone)
     except:
         pass
 
 
-    cv2.putText(frame_movement, str("mouvement" + " " + str(zone)),
-                (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(255,255,255),1,cv2.LINE_AA)
+    if len(ll1[0]) < 2:
+        init_zones(x, y, w, h, frame_movement,
+                   ll1, ll2, ll3, ll4, ll5, ll6, ll7, ll8,
+                   ll9, ll10, ll11, ll12, ll13, ll14, ll15)
 
 
-    DIRECTION_VERTICALE.append(y+h)
+    if len(ll1[0]) >= 2:
+        zones(frame_movement,
+              ll1, ll2, ll3, ll4, ll5, ll6, ll7, ll8,
+              ll9, ll10, ll11, ll12, ll13, ll14, ll15)
 
 
 
-    return True
+
+
+
+
+
+
+
 
     
+    originale = gray
+    cv2.imshow("frame_movement", frame_movement)
+    
+    counter+=1
+        
+
+
+    key=cv2.waitKey(300)&0xFF
+    if key==ord('q'):
+        break
 
 
 
 
-
-
-
-    #IDEE IA
-    #Le but serait de lui dire -> RETIENT TOUS LES CADRES FREDO
-    #si un cadre pop maintenant dans les + ou - 20 px
-    #ALORS c la main
-    #en arretant toutes les conditions passées.
-
-    #CONDITION la vidéo doit durée + de 20 minutes et pas de plan coupé.
-    #Ne marche qu'avec le bas et pas la tete
-
-
-
-
-
-#--------------------------------------------------FIGURE DETECTION
-
-def detection(mask, l, phrase, nb, x):
-    """Here we ask the sum of the list(we insert element in list
-    from the box). if the mean > + 50000 there is movement in there"""
-    try:
-        #doing the sum of elements of the list
-        liste = sum([j for i in mask for j in i])
-        #we add the sum of all element
-        l.append(liste)
-        #if the current list of detection is > at the mean of all passation
-        #a refaire
-        if liste > sum(l)/len(l) + nb:
-            #we return the point detected
-            return phrase
-    except:
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+cap.release()
+cv2.destroyAllWindows()
